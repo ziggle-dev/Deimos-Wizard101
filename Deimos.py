2258426a8a9ce68a2a68a6a19f427c47e9a6e4d4
Slackaduts <54456755+Slackaduts@users.noreply.github.com>
2025-02-24 19:09:41 -0500

    Prepped repo for 3.9.1 release

--- Deimos.py
+++ Deimos.py
@@ -51,7 +51,7 @@ from src.deimoslang import vm
 cMessageBox = ctypes.windll.user32.MessageBoxW
 
 
-tool_version: str = '3.9.0'
+tool_version: str = '3.9.1'
 tool_name: str = 'Deimos'
 tool_author: str = 'Deimos-Wizard101'
 repo_name: str = tool_name + '-Wizard101'
